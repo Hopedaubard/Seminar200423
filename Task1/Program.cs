a2fbf44767b147d0c4fad1b048f3363967daaff4
Alex <trainofbird@gmail.com>
2023-04-20 22:42:35 +0300

    task1 is ended

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает 
+// два числа и проверяет, является ли 
+// первое число квадратом второго.
+
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num2*num2==num1)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+// для одной строки в теле программы фигурные скобочки не нужны
+// по-хорошему фигурные скобочки нужны здесь для читабельности
